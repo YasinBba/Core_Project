2491e76b238446464ffb351804667ebc59f9b07b
Yasinus <Yasinus@DESKTOP-29IUMJH>
2025-11-14 16:38:55 +0300

    entityler tanımlanıyor

---
+++ EntityLayer/Concrete/Portfolio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Portfolio
+    {
+        public int PortfolioId { get; set; }
+        public string Name { get; set; }
+        public string İmageUrl { get; set; }
+        
+    }
+}
